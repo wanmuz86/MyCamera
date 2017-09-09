c1b1a8caf0c82200d877cd6c0494e9432e5cb519
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2017-09-09 16:56:58 +0800

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+	image
+	options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.DATA_URL,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+}
+
+  constructor(public navCtrl: NavController, private camera: Camera, private socialSharing : SocialSharing) {
 
   }
 
+facebookTapped(){
+	this.socialSharing.shareViaFacebook('our first app', this.image).then(() => {
+  // Success!
+}).catch(() => {
+  // Error!
+});
+
+}
+  cameraTapped(){
+	this.camera.getPicture(this.options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64:
+ let base64Image = 'data:image/jpeg;base64,' + imageData;
+ this.image = base64Image
+}, (err) => {
+
+});
+}
 }
